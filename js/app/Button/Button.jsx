f266a2bb6b666dffbb204c907bca1e6d66f4bd27
Florian Didron <florian@111studio.jp>
2016-08-28 18:51:08 +0900

    Makes button stateful

--- js/app/Button/Button.jsx
+++ js/app/Button/Button.jsx
@@ -3,10 +3,15 @@ import './Button.less';
 import {Row, Item, Column} from 'react-plan';
 
 type Props = {
-
+  state: string
 }
 
 const baseClass = 'button-base';
+
+const strings = {
+  saving: 'Saving ...',
+  success: 'Complete'
+};
 
 class Button extends React.Component {
 
@@ -31,9 +36,24 @@ class Button extends React.Component {
   //componentWillUnmount() {}
   render() {
     let { className, ...others } = this.props;
+    let children = null;
+    let disabled = false;
     className = (className) ? `${baseClass} ${className}` : baseClass;
+    switch (this.props.state) {
+      case 'SAVING':
+        disabled = true;
+        children = <span><i span data-icon="z" className="icon"></i> { strings.saving }</span>;
+        break;
+      case 'SUCCESS':
+        disabled = true;
+        children = <span><i className="icon-check"></i> { strings.success }</span>;
+        break;
+      default:
+        children = this.props.children;
+        break;
+    }
     return (
-      <button className={ className } { ...others } >{ this.props.children }</button>
+      <button className={ className } { ...others } disabled={ disabled }>{ children }</button>
     );
   }
   /* End React Component LifeCycle Methods */
